24fd924e9724086859aca3f7dc80cb685a77c9b9
Manuel Goulão <msilvagoulao@gmail.com>
2020-06-01 19:48:19 +0100

    add footer, more changes to responsiveness

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,43 +6,47 @@ import Link from "next/link";
 export const siteTitle = "CS Essentials by IEEE-IST CS";
 
 export default function Layout({
-  children,
-  home,
+	children,
+	home,
 }: {
-  children: React.ReactNode;
-  home?: boolean;
+	children: React.ReactNode;
+	home?: boolean;
 }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content=""
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <Header />
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="description" content="" />
+				<meta
+					property="og:image"
+					content={`https://og-image.now.sh/${encodeURI(
+						siteTitle
+					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+				/>
+				<meta name="og:title" content={siteTitle} />
+				<meta name="twitter:card" content="summary_large_image" />
+			</Head>
+			<Header />
 
-      <div className={styles.mainContent}>
-        {!home ? <div className={styles.backToHome}>
-          <Link href="/">
-            <a>
-              <img src="/icons/chevron--left.svg" alt="back-icon" />
-              <span>Back to Home</span>
-            </a>
-          </Link>
-        </div> : ""}
+			<div className={styles.mainContent}>
+				{!home ? (
+					<div className={styles.backToHome}>
+						<Link href="/">
+							<a>
+								<img src="/icons/chevron--left.svg" alt="back-icon" />
+								<span>Back to Home</span>
+							</a>
+						</Link>
+					</div>
+				) : (
+					""
+				)}
 
-        <main>{children}</main>
-      </div>
-    </div>
-  );
+				<main>{children}</main>
+			</div>
+			<footer className={styles.footer}>
+				<span className={styles.footerTitle}>{siteTitle}</span>
+			</footer>
+		</div>
+	);
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -2,35 +2,45 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { getPages } from "../lib/content";
 import { GetStaticProps } from "next";
+import styles from "../components/layout.module.css";
+
 import { useRouter } from "next/router";
 import { search } from "../lib/stringSearch";
 import SearchCard from "../components/cards/searchCard";
 
 export default function Search({ pages }: { pages: string[] }) {
-  const router = useRouter();
-  let nColor = 0;
-  const MAX_COLOR = 7;
-  let resultPages: {title: string, ratio: number}[] = [];
-  if (router.query.query !== undefined)
-    resultPages = search(router.query.query as string, pages);
+	const router = useRouter();
+	let nColor = 0;
+	const MAX_COLOR = 7;
+	let resultPages: { title: string; ratio: number }[] = [];
+	if (router.query.query !== undefined)
+		resultPages = search(router.query.query as string, pages);
 
-  return (
-    <Layout>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      {resultPages.map((value) => {
-        return (<SearchCard name={value.title} key={value.title} color={`backColor${(nColor = (nColor % MAX_COLOR) + 1)}`}/>);
-      })}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<div className={styles.contentContainer}>
+				{resultPages.map((value) => {
+					return (
+						<SearchCard
+							name={value.title}
+							key={value.title}
+							color={`backColor${(nColor = (nColor % MAX_COLOR) + 1)}`}
+						/>
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const pages = await getPages();
-  return {
-    props: {
-      pages,
-    },
-  };
+	const pages = await getPages();
+	return {
+		props: {
+			pages,
+		},
+	};
 };
