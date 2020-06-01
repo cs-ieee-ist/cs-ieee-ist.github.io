73b5acc91c3f493b07c1283f258d0383eec0d494
Manuel Goulão <msilvagoulao@gmail.com>
2020-06-01 19:57:45 +0100

    fix recent changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,24 +2,29 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { getAboutContent } from "../lib/content";
 import ContentCard from "../components/cards/contentCard";
+import styles from "../components/layout.module.css";
 import { GetStaticProps } from "next";
 
 export default function About({ aboutContent }: { aboutContent: string }) {
-  return (
-    <Layout>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-        <ContentCard><div dangerouslySetInnerHTML={{ __html: aboutContent }} /></ContentCard>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<div className={styles.contentContainer}>
+				<ContentCard>
+					<div dangerouslySetInnerHTML={{ __html: aboutContent }} />
+				</ContentCard>
+			</div>
+		</Layout>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const aboutContent = await getAboutContent();
-  return {
-    props: {
-      aboutContent,
-    },
-  };
+	const aboutContent = await getAboutContent();
+	return {
+		props: {
+			aboutContent,
+		},
+	};
 };
